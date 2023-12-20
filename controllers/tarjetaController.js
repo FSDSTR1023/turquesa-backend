a7ebbf0a18d1c234386bd2235b295e3d5a580ab4
Juan Mencía <juamm99@gmail.com>
2023-12-20 20:24:34 +0100

    CRUD de la tarjeta creado

---
+++ controllers/tarjetaController.js
@@ -0,0 +1,59 @@
+const Tarjeta = require('../models/task.model');
+
+async function getTarjetas(req,res) {
+    Tarjeta.find()
+        .then(tarjetas => {
+            console.log('Tarjetas encontradas: ', tarjetas)
+            res.status(200).json(tarjetas)
+        })
+        .catch(err => {
+            console.log('Error al recuperar las tarjetas: ', err)
+            res.status(400).json(err)
+        });
+}
+
+async function updateTarjeta(req,res) {
+    const tarjetaAActualizar = req.body;
+    Tarjeta.findByIdAndUpdate(req.params.id, {$set: tarjetaAActualizar})
+        .then(tarjeta => {
+            console.log('Tarjeta actualizada: ', tarjeta)
+            res.status(200).json(tarjeta)
+        })
+        .catch(err => {
+            console.log('Error al actualizar la tarjeta: ', err)
+            res.status(400).json(err)
+        });
+}
+
+async function crearTarjeta(req, res) {
+    const tarjeta= req.body;
+    tarjeta.id = Math.random().toString(36);
+    Tarjeta.create(tarjeta)
+    .then(tarjeta => {
+        console.log(`Tarjeta creada: ${tarjeta}`)
+        res.status(200).json(tarjeta)
+    })
+    .catch(err => {
+        console.log('Error al crear la tarjeta: ', err)
+        res.status(400).json(err)
+    });
+}
+
+async function borrarTarjeta(req,res) {
+    Tarjeta.findByIdAndDelete(req.params.id)
+        .then(tarjeta => {
+            console.log('Tarjeta borrada: ', tarjeta)
+            res.status(200).json(tarjeta)
+        })
+        .catch(err => {
+            console.log('Error al borrar la tarjeta: ', err)
+            res.status(400).json(err)
+        });
+}
+
+module.exports = {
+    getTarjetas,
+    updateTarjeta,
+    crearTarjeta,
+    borrarTarjeta
+}
