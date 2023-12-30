cc55913364dc1532bdfd702be94e909049c314a8
Juan Mencía <juamm99@gmail.com>
2023-12-30 22:58:45 +0100

    Controladores para Asistente y Usuario creados

---
+++ controllers/asistenteController.js
@@ -0,0 +1,59 @@
+const Asistente = require('../models/asistente.model');
+
+async function getAsistentes(req,res) {
+    Asistente.find()
+        .then(tarjetas => {
+            console.log('Asistentes encontrados: ', tarjetas)
+            res.status(200).json(tarjetas)
+        })
+        .catch(err => {
+            console.log('Error al recuperar los asistentes: ', err)
+            res.status(400).json(err)
+        });
+}
+
+async function updateAsistente(req,res) {
+    const asistenteAActualizar = req.body;
+    Asistente.findByIdAndUpdate(req.params.id, {$set: asistenteAActualizar})
+        .then(asistente => {
+            console.log('asistente actualizado: ', asistente)
+            res.status(200).json(asistente)
+        })
+        .catch(err => {
+            console.log('Error al actualizar el asistente: ', err)
+            res.status(400).json(err)
+        });
+}
+
+async function crearAsistente(req, res) {
+    const asistente= req.body;
+    asistente.id = Math.random().toString(36);
+    Asistente.create(asistente)
+    .then(asistente => {
+        console.log(`Asistente creado: ${asistente}`)
+        res.status(200).json(asistente)
+    })
+    .catch(err => {
+        console.log('Error al crear el asistente: ', err)
+        res.status(400).json(err)
+    });
+}
+
+async function borrarAsistente(req,res) {
+    Asistente.findByIdAndDelete(req.params.id)
+        .then(asistente => {
+            console.log('Asistente borrada: ', asistente)
+            res.status(200).json(asistente)
+        })
+        .catch(err => {
+            console.log('Error al borrar el asistente: ', err)
+            res.status(400).json(err)
+        });
+}
+
+module.exports = {
+    getAsistentes,
+    updateAsistente,
+    crearAsistente,
+    borrarAsistente
+}
---
+++ controllers/usuarioController.js
@@ -0,0 +1,59 @@
+const Usuario = require('../models/usuario.model');
+
+async function getUsuarios(req,res) {
+    Usuario.find()
+        .then(usuarios => {
+            console.log('Usuarios encontrados: ', usuarios)
+            res.status(200).json(usuarios)
+        })
+        .catch(err => {
+            console.log('Error al recuperar los usuarios: ', err)
+            res.status(400).json(err)
+        });
+}
+
+async function updateUsuario(req,res) {
+    const usuarioAActualizar = req.body;
+    Usuario.findByIdAndUpdate(req.params.id, {$set: usuarioAActualizar})
+        .then(usuario => {
+            console.log('Usuario actualizado: ', usuario)
+            res.status(200).json(usuario)
+        })
+        .catch(err => {
+            console.log('Error al actualizar el usuario: ', err)
+            res.status(400).json(err)
+        });
+}
+
+async function crearUsuario(req, res) {
+    const usuario= req.body;
+    usuario.id = Math.random().toString(36);
+    Usuario.create(usuario)
+    .then(usuario => {
+        console.log(`Usuario creada: ${usuario}`)
+        res.status(200).json(usuario)
+    })
+    .catch(err => {
+        console.log('Error al crear el usuario: ', err)
+        res.status(400).json(err)
+    });
+}
+
+async function borrarUsuario(req,res) {
+    Usuario.findByIdAndDelete(req.params.id)
+        .then(usuario => {
+            console.log('Usuario borrado: ', usuario)
+            res.status(200).json(usuario)
+        })
+        .catch(err => {
+            console.log('Error al borrar el usuario: ', err)
+            res.status(400).json(err)
+        });
+}
+
+module.exports = {
+    getUsuarios,
+    updateUsuario,
+    crearUsuario,
+    borrarUsuario
+}
