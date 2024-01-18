a97d00a169b4d0c2c052fcd8ab7d35093447b4c7
DanielAle98 <148280318+DanielAle98@users.noreply.github.com>
2024-01-18 21:46:33 +0100

    Update app.js
    Autenticacion login

--- app.js
+++ app.js
@@ -40,4 +40,45 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
 
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!user) {
+    res.status(401).send({ error: "User not found" });
+    return;
+  }
+
+  jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET,
+    (err, token) => {
+      if (err) {
+        res.status(401).send({ error: err.message });
+      } else {
+        res
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            expires: new Date("2100-12-17T03:24:00"),
+          })
+          .status(201)
+          .send();
+        //res.status(201).send({ token });
+      }
+    }
+  );
+});
+
+app.post("/api/logout", (_req, res) => {
+  res.clearCookie("token").send();
+});
+
+app.get("/api/profile", authMiddleware, (req, res) => {
+  const user = users.find((user) => user.id === req.user.id);
+  res.json({ ...user, password: undefined });
+});
+
 module.exports = app;
