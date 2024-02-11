0619b4107503dc1915311c769fa40416ef6dbd05
m-marinucci <mmarinucci@numinate.com>
2024-02-11 02:05:45 +0100

    creado script para subir tarjetas nuevas y end- point para visualizar todas las tarjetas sin filtros.

--- routes/tarjeta.js
+++ routes/tarjeta.js
@@ -5,14 +5,17 @@ var router = express.Router();
 
 const controladorTarjetas = require('../controllers/tarjetaController');
 
+// General routes
 router.get('/', controladorTarjetas.getTarjetasMuestra);
-
-router.get('/:id', controladorTarjetas.getTarjeta);
-
-router.put('/:id', controladorTarjetas.updateTarjeta);
-
 router.post('/', controladorTarjetas.crearTarjeta);
 
+// Specific routes
+router.get('/all', controladorTarjetas.getAllTarjetas);
+router.get('/usuario/:userId', controladorTarjetas.getTarjetasPorUsuario);
+
+// ID-specific routes
+router.get('/:id', controladorTarjetas.getTarjeta);
+router.put('/:id', controladorTarjetas.updateTarjeta);
 router.delete('/:id', controladorTarjetas.borrarTarjeta);
 
 module.exports = router;
