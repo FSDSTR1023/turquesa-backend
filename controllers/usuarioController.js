708358aabfb6853974a5cb5588a601b6705f469f
m-marinucci <mmarinucci@numinate.com>
2024-02-21 21:00:35 +0100

    commit deletion

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -119,46 +119,6 @@ async function checkUserSaved(req, res) {
     res.json({ usuarioRecuperado, password: undefined });
 }
 
-// app.post("/api/login", (req, res) => {
-//   const { username, password } = req.body;
-
-//   const user = users.find(
-//     (user) => user.username === username && user.password === password
-//   );
-//   if (!user) {
-//     res.status(401).send({ error: "User not found" });
-//     return;
-//   }
-
-//   jwt.sign(
-//     { id: user.id, username: user.username },
-//     process.env.JWT_SECRET,
-//     (err, token) => {
-//       if (err) {
-//         res.status(401).send({ error: err.message });
-//       } else {
-//         res
-//           .cookie("token", token, {
-//             httpOnly: true,
-//             secure: false,
-//             expires: new Date("2100-12-17T03:24:00"),
-//           })
-//           .status(201)
-//           .send();
-//         //res.status(201).send({ token });
-//       }
-//     }
-//   );
-// });
-
-// app.post("/api/logout", (_req, res) => {
-//   res.clearCookie("token").send();
-// });
-
-// app.get("/api/profile", authMiddleware, (req, res) => {
-//   const user = users.find((user) => user.id === req.user.id);
-//   res.json({ ...user, password: undefined });
-// });
 
 module.exports = {
     getUsuarios,
