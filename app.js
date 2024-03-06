6a34bdfcc732c54704a1e9eaf9cc66e977ad8827
m-marinucci <mmarinucci@numinate.com>
2024-03-06 20:26:42 +0100

    last

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const port = 3000;
 const cors = require('cors');
 
 app.use(cors({
-  origin: {'https://cloudinvito.netlify.app',  },
+  origin: 'https://cloudinvito.netlify.app',
   credentials: true,
 }));
 
