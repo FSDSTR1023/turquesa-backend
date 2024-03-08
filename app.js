a897602ecb9ac288a680bc156cb52db0cbb67d6e
Juan Mencía <juamm99@gmail.com>
2024-03-08 12:12:52 +0100

    port changed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cookieParser =  require("cookie-parser");
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 const cors = require('cors');
 app.use(cors({
   origin:'https://cloudinvito.netlify.app/',
