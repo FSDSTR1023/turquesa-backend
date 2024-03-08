6d154e38fae179a22d43ddca7b4d825c52754ece
Juan Mencía <juamm99@gmail.com>
2024-03-08 12:12:19 +0100

    Changes to deploy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin:'https://cloudinvito.netlify.app/',
   credentials: true,
 }));
 app.use(cookieParser());
